471958a569f8f5fbb33d892de67417d7e4c5178d
vikpan1308gbglj-dotcom <vikpan1308gbglj@gmail.com>
2025-10-16 07:37:43 +0530

    Port Setting Update for Flask

--- Enhanced_DNS_Lookup_WebApp.py
+++ Enhanced_DNS_Lookup_WebApp.py
@@ -558,4 +558,6 @@ def run_dns_lookup(input_csv_path, output_dir):
             log_file]
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
